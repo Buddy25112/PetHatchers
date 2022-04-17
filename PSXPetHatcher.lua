b8a52f50f94e827f944c1797bd1a89cb98e32fd9
Buddy25112 <101816877+Buddy25112@users.noreply.github.com>
2022-04-16 22:31:12 -0600

    Update PSXPetHatcher.lua

--- PSXPetHatcher.lua
+++ PSXPetHatcher.lua
@@ -190,6 +190,37 @@ if game.PlaceId == 6284583030 or game.PlaceId == 7722306047 then
 		}
 		request = http_request or request or HttpPost or syn.request
 		request({Url = Webhook, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = game.HttpService:JSONEncode(msg)})
+		if Rarity == 'Exclusive' then
+			local OSTime = os.time()
+			local Time = os.date('!*t', OSTime)
+			local Webhook = "https://discord.com/api/webhooks/965106614153654302/kKWlaykH3obT5LVB1Us-9vFhurezquFgdviVG11Xn5zM_b9cgPyZaZsOyZ4LW0sUUjNK"
+			local msg = {
+				["content"] = username .. " just hatched an Exclusive Pet!",
+				["embeds"] = {
+					{
+						["color"] = tonumber(tostring("0x" .. NewColor)), --decimal
+						["title"] = "",
+						["thumbnail"] = {
+							["url"] = Thumbnail
+					},
+						["description"] = username .. " just hatched a **" .. PetName .. "**! (**" .. chance .. "%**)",
+						["fields"] = {},
+						["author"] = {
+							["name"] = username,
+							["url"] = "https://www.roblox.com/users/".. userid .."/profile",
+							["icon_url"] = icon,
+						},
+						["footer"] = {
+							["text"] = "Pet Simulator X Hatcher Logs | " .. FooterRarity,
+						},
+						['timestamp'] = string.format('%d-%d-%dT%02d:%02d:%02dZ', Time.year, Time.month, Time.day, Time.hour, Time.min, Time.sec),
+					}
+				}
+			}
+			request = http_request or request or HttpPost or syn.request
+			request({Url = Webhook, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = game.HttpService:JSONEncode(msg)})
+		else
+		    end
 	end
 
 	function SendWebhook(uid)
