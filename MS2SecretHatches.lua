448ce0917fa10a1edc01a434044359bef99492a3
Buddy25112 <101816877+Buddy25112@users.noreply.github.com>
2022-07-16 15:17:34 -0600

    Create MS2SecretHatches.lua

---
+++ MS2SecretHatches.lua
@@ -0,0 +1,169 @@
+-- Formatting Counts
+function abb(Value)
+    local Number
+    local Formatted = Value
+    while true do
+        Formatted, Number = string.gsub(Formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
+        if (Number == 0) then
+            break
+        end
+    end
+    return Formatted
+end
+
+-- Connecting to chat
+local Headers = {["content-type"] = "application/json"}
+local Chat = game:GetService("Players").LocalPlayer.PlayerGui.Chat.Frame.ChatChannelParentFrame["Frame_MessageLogDisplay"].Scroller
+
+-- The Main Function
+Chat.ChildAdded:Connect(function(instance)
+    if string.find(instance.TextLabel.Text,"Server") then
+            if string.find(instance.TextLabel.Text,"just") then
+                if string.find(instance.TextLabel.Text,"hatched") then
+                    if string.find(instance.TextLabel.Text,"a") then
+                        
+                        -- Get Hatcher Info
+                        local OnlyCount1
+                        local username
+                        if string.find(instance.TextLabel.Text, "buddy25112") then
+                                OnlyCount1 = "buddy25112"
+                                username = game:GetService("Players").buddy25112.Name
+                                
+                            elseif string.find(instance.TextLabel.Text, "buddy25113") then
+                                OnlyCount1 = "buddy25113"
+                                username = game:GetService("Players").buddy25113.Name
+                                
+                            elseif string.find(instance.TextLabel.Text, "BuddyHatchAlt4") then
+                                OnlyCount1 = "BuddyHatchAlt4"
+                                username = game:GetService("Players").BuddyHatchAlt4.Name
+                                
+                            elseif string.find(instance.TextLabel.Text, "BuddyHatchAlt1") then
+                                OnlyCount1 = "BuddyHatchAlt1"
+                                username = game:GetService("Players").BuddyHatchAlt1.Name
+                                
+                            elseif string.find(instance.TextLabel.Text, "AJ_GamingYYTT") then
+                                OnlyCount1 = "AJ_GamingYYTT"
+                                username = game:GetService("Players").AJ_GamingYYTT.Name
+
+                            end
+                            
+                        local OnlyCount = {OnlyCount1}
+                        
+                        -- Get Counts
+                        local count = 0
+                        for i,v in pairs(game:GetService("Players"):GetChildren()) do
+                            for n,b in pairs(OnlyCount) do
+                                if string.find(v.Name, b) then
+                        	        count = v.leaderstats["Eggs"].Value    
+                                end
+                            end
+                        end
+                        
+                        -- Get Pet Info
+                        local PetChance
+                        local PetImage
+                        local petname
+                        if string.find(instance.TextLabel.Text, "Chocolate Sundae") then
+                            if string.find(instance.TextLabel.Text, "Shiny") then
+                                PetChance = "1/800M"
+                                petname = "Shiny Chocolate Sundae"
+                                PetImage = "https://media.discordapp.net/attachments/749822808288395354/997681479175897098/Chocolate_Sundae.png"
+                            else
+                                PetChance = "1/8.00M"
+                                petname = "Chocolate Sundae"
+                                PetImage = "https://media.discordapp.net/attachments/749822808288395354/997681479175897098/Chocolate_Sundae.png"
+                            end
+                        
+                        elseif string.find(instance.TextLabel.Text, "Vanilla Sundae") then
+                            if string.find(instance.TextLabel.Text, "Shiny") then
+                                PetChance = "1/800M"
+                                petname = "Shiny Vanilla Sundae"
+                                PetImage = "https://media.discordapp.net/attachments/749822808288395354/997681477942784080/Vanilla_Sundae.png"
+                            else
+                                PetChance = "1/8.00M"
+                                petname = "Vanilla Sundae"
+                                PetImage = "https://media.discordapp.net/attachments/749822808288395354/997681477942784080/Vanilla_Sundae.png"
+                            end
+                                
+                        elseif string.find(instance.TextLabel.Text, "Strawberry Sundae") then
+                            if string.find(instance.TextLabel.Text, "Shiny") then
+                                PetChance = "1/800M"
+                                petname = "Shiny Strawberry Sundae"
+                                PetImage = "https://media.discordapp.net/attachments/749822808288395354/997681478299295754/Strawberry_Sundae.png"
+                            else
+                                PetChance = "1/8.00M"
+                                petname = "Strawberry Sundae"
+                                PetImage = "https://media.discordapp.net/attachments/749822808288395354/997681478299295754/Strawberry_Sundae.png"
+                            end
+                        
+                        elseif string.find(instance.TextLabel.Text, "Mint Sundae") then
+                            if string.find(instance.TextLabel.Text, "Shiny") then
+                                PetChance = "1/800M"
+                                petname = "Shiny Mint Sundae"
+                                PetImage = "https://media.discordapp.net/attachments/749822808288395354/997681478639038514/Mint_Sundae.png"
+                            else
+                                PetChance = "1/8.00M"
+                                petname = "Mint Sundae"
+                                PetImage = "https://media.discordapp.net/attachments/749822808288395354/997681478639038514/Mint_Sundae.png"
+                            end
+                        end
+                        
+                        -- Other Features
+                        local OSTime = os.time()
+                        local Webhook = _G.Webhooksss
+                        local Time = os.date('!*t', OSTime)
+                        
+                        -- Shiny/Normal Secret Info
+                        local NewColor
+                        local SecretPing
+                        local TitleFinder
+                        local FooterFinder
+                        if string.find(instance.TextLabel.Text,"Secret Shiny") then
+                            SecretPing = "<@&928384646595641404> <@&803012562878726174>, a **Shiny** **Secret** has been hatched!"
+                            TitleFinder = "A Shiny Secret was just hatched!"
+                            FooterFinder = "Shiny Secret"
+                            NewColor = "F7FF00"
+                        else
+                            SecretPing = "<@&803012562878726174>, a **Secret** has been hatched!"
+                            TitleFinder = "A Secret was just hatched!"
+                            FooterFinder = "Secret"
+                            NewColor = "FF00E6"
+                        end
+                        
+                        -- The Webhook
+                            local Info = {
+                                ["content"] = SecretPing,
+                                ["embeds"] = {
+                                {
+                                ["description"] = username .. " just hatched a **" .. petname .. "**!",
+                                ["author"] = {},
+                                ["thumbnail"] = {
+                                    ["url"] = PetImage
+                                },
+                                ["footer"] = {
+                                    ["text"] = "Egg #" .. abb(count) .. " | " .. FooterFinder,
+                                },
+                                ['timestamp'] = string.format('%d-%d-%dT%02d:%02d:%02dZ', Time.year, Time.month, Time.day, Time.hour, Time.min, Time.sec),
+                                ["title"] = TitleFinder,
+                                ["color"] = tonumber(tostring("0x" .. NewColor)), --decimal FF00E6
+                                ["fields"] = {
+						        {
+							        ["name"] = "Chance:",
+							        ["value"] = PetChance,
+							        ["inline"] = true
+						        },
+						        {
+							        ["name"] = "Hatcher:",
+							        ["value"] = username,
+							        ["inline"] = true
+						        },
+					        },}}}
+                            local Info = game:GetService("HttpService"):JSONEncode(Info)
+                            local HttpRequest = http_request;
+                            if syn then HttpRequest = syn.request else HttpRequest = http_request end
+                                HttpRequest({Url=Webhook, Body=Info, Method="POST", Headers=Headers})
+                        end
+                    end
+                end
+            end
+end)
